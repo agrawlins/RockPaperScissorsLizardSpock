5f196bf44d19991241c3ebf47112331efef39953
agrawlins <agrawlins@gmail.com>
2021-07-06 21:45:53 -0400

    Oops! Missed one

--- Player.js
+++ Player.js
@@ -1,15 +1,22 @@
 //Import Human.js and AI.js
 
 //Create class for Player (Human-OR-AI)
-class Player {
+class Player1 {
+    nameOfPlayer1 = prompt("Enter your username:");
+    layer1 = nameOfPlayer1;
+}
+
+class Player2 {
     constructor(){
-        nameOfPlayer1 = prompt("Enter your username:");
-        player1 = nameOfPlayer1;
         nameOfPlayer2 = selectHumanOrAI();
         player2 = nameOfPlayer2;
     }
 }
 
-
+function selectHumanOrAI(){
+    console.log("What game mode are you playing? \n 1) Singleplayer \n 2) Multiplayer?")
+    let playerType =
+    
+}
 //Import selections from Human.js and AI.js
 //Export selections to Game.js
